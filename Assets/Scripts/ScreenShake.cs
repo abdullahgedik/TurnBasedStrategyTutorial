957acf2f93c13df787029de86fa524ffff1649fe
Abdullah Gedik <113609377+abdullahgedik@users.noreply.github.com>
2024-08-02 17:17:13 +0300

    Add: Camera Shake

---
+++ Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Cinemachine;
+
+public class ScreenShake : MonoBehaviour
+{
+    public static ScreenShake Instance { get; private set; }
+
+    private CinemachineImpulseSource cinemachineImpulseSource;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one Screenshake! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    public void Shake(float intensity = 1f)
+    {
+        cinemachineImpulseSource.GenerateImpulse(intensity);
+    }
+}
