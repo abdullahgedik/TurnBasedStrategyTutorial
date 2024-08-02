429798d8a189da059066ddf8899e4e5a09e79416
Abdullah Gedik <113609377+abdullahgedik@users.noreply.github.com>
2024-08-02 13:48:41 +0300

    Add: Pathfinding Implementation

--- Assets/Scripts/PathNode.cs
+++ Assets/Scripts/PathNode.cs
@@ -30,4 +30,37 @@ public class PathNode
     {
         return fCost;
     }
+
+    public void SetGCost(int gCost)
+    {
+        this.gCost = gCost;
+    }
+    public void SetHCost(int hCost)
+    {
+        this.hCost = hCost;
+    }
+    public void CalculateFCost()
+    {
+        this.fCost = gCost + hCost;
+    }
+
+    public PathNode GetCameFromPathNode()
+    {
+        return cameFromPathNode;
+    }
+
+    public void ResetCameFromPathnode()
+    {
+        cameFromPathNode = null;
+    }
+
+    public void SetCameFromPathNode(PathNode pathNode)
+    {
+        cameFromPathNode = pathNode;
+    }
+
+    public GridPosition GetGridPosition()
+    {
+        return gridPosition;
+    }
 }
